24d84b907d7dc6ac4361021d28a4feb0b23e560d
thom krupa <thom@bejamas.com>
2026-01-10 16:43:36 +0100

    refactor: change part api to multiple files and barrel index

---
+++ packages/ui/src/components/avatar/index.ts
@@ -0,0 +1,4 @@
+export { default as Avatar } from "./Avatar.astro";
+export { default as AvatarImage } from "./AvatarImage.astro";
+export { default as AvatarFallback } from "./AvatarFallback.astro";
+
---
+++ packages/ui/src/components/footer/index.ts
@@ -0,0 +1,10 @@
+export { default as Footer } from "./Footer.astro";
+export { default as FooterTop } from "./FooterTop.astro";
+export { default as FooterMain } from "./FooterMain.astro";
+export { default as FooterBottom } from "./FooterBottom.astro";
+export { default as FooterContainer } from "./FooterContainer.astro";
+export { default as FooterGrid } from "./FooterGrid.astro";
+export { default as FooterRow } from "./FooterRow.astro";
+export { default as FooterLogo } from "./FooterLogo.astro";
+export { default as FooterLegal } from "./FooterLegal.astro";
+
