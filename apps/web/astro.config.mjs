9c93f0467e6931a31967e37bc3e9a27be7a4b3e3
thom krupa <thom@bejamas.com>
2026-02-05 09:08:08 +0100

    chore(docs): add dark mode to the nav

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -84,6 +84,7 @@ export default defineConfig({
           label: "Concepts",
           items: [
             "docs/theming",
+            "docs/dark-mode",
             "docs/design-principles",
             "docs/monorepo",
             "docs/auto-generated-docs",
