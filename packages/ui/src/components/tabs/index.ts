0452d76b17597bb71f08a279a4931fbf3ae195a1
thom krupa <thom@bejamas.com>
2026-02-18 11:26:23 +0100

    fix: improve tabs styling

--- packages/ui/src/components/tabs/index.ts
+++ packages/ui/src/components/tabs/index.ts
@@ -2,4 +2,4 @@ export { default as Tabs } from "./Tabs.astro";
 export { default as TabsList } from "./TabsList.astro";
 export { default as TabsTrigger } from "./TabsTrigger.astro";
 export { default as TabsContent } from "./TabsContent.astro";
-export { default as TabsIndicator } from "./TabsIndicator.astro";
+export { tabsListVariants } from "./TabsList.astro";
